6bf34e58785d67ecec5bdb7b41d0c0a54240889d
Steven Looman <steven.looman@gmail.com>
2012-01-28 13:19:08 +0100

    Added GeoJSON parser/outputter

--- example.js
+++ example.js
@@ -1,9 +1,9 @@
-var Nen1878Reader = require('./');
+var nen1878reader = require('./');
 
 function main() {
-    //var reader = new Nen1878Reader('test_data/WEESP_N__7001.NEN');
-    //var reader = new Nen1878Reader('test_data/dummy.nen');
-    var reader = new Nen1878Reader('test_data/7500_09.sfn');
+    var reader = new nen1878reader.Nen1878Reader('test_data/WEESP_N__7001.NEN');
+    //var reader = new nen1878reader.Nen1878Reader('test_data/dummy.nen');
+    //var reader = new nen1878reader.Nen1878Reader('test_data/7500_09.sfn');
 
     reader.on('record', onRecord);
     reader.on('end', onEnd);
@@ -21,6 +21,10 @@ function onRecord(record) {
 
     var lkiCode = record.lki_code;
     lkiCodes[lkiCode] = (lkiCodes[lkiCode] || 0) + 1;
+
+    if (record.recordType == 3) {
+        var geoJson = nen1878reader.GeoJson.extractLineString(record);
+    }
 }
 
 function onEnd() {
