8642b48ffc844f33f606fc602e6a302dca9f4505
Steven Looman <steven.looman@gmail.com>
2013-09-22 19:35:02 +0200

    EPSG 28992 expects [m] instead of [mm]

--- jsoner.js
+++ jsoner.js
@@ -26,6 +26,14 @@ function onRecord(record) {
     if (record.recordType == 3 || record.recordType == 5) {
         var feature = nen1878reader.GeoJson.toFeature(record);
 
+        // NEN1878 coordinates are in [mm], EPSG:28992 expects [m]
+        if (feature.geometry.type === 'Point') {
+            feature.geometry.coordinates = feature.geometry.coordinates.map(function(ord) { return ord / 1000.0; });
+        } else if (feature.geometry.type === 'LineString') {
+            feature.geometry.coordinates = feature.geometry.coordinates.map(function(coord) { return [ coord[0] / 1000.0, coord[1] / 1000.0 ]; });
+        }
+        feature.bbox = feature.bbox.map(function(c) { return c / 1000.0; });
+
         if (isFirst) {
             isFirst = false;
         } else {
